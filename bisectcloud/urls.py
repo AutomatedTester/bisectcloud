f317e7506528a760c1f071d5281c5fbffcfbb53b
AutomatedTester <dburns@mozilla.com>
2013-03-25 17:01:13 +0000

    creating new site app for bisect

--- bisectcloud/urls.py
+++ bisectcloud/urls.py
@@ -1,35 +1,23 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.defaults import *
 
-from .examples import urls
+from .site import views
 
-from funfactory.monkeypatches import patch
-patch()
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    (r'', include(urls)),
-    
-    # Generate a robots.txt
-    (r'^robots\.txt$', 
-        lambda r: HttpResponse(
-            "User-agent: *\n%s: /" % 'Allow' if settings.ENGAGE_ROBOTS else 'Disallow' ,
-            mimetype="text/plain"
-        )
-    )
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='site.home'),
+    url(r'^browserid/', include('django_browserid.urls')),
+    url(r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/'},
+        name='examples.logout'),
+    url(r'^bleach/?$', views.bleach_test, name='examples.bleach'),
 )
-
-## In DEBUG mode, serve media files through Django.
 if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns(
+        '',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
+    )
     urlpatterns += staticfiles_urlpatterns()
