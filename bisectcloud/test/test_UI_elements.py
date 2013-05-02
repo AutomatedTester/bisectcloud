3cbb38e28ead7efaffd852c32c84ca3b66acd1e9
AutomatedTester <dburns@mozilla.com>
2013-05-02 14:55:35 +0100

    adding fixture and updating the test

--- bisectcloud/test/test_UI_elements.py
+++ bisectcloud/test/test_UI_elements.py
@@ -1,8 +1,11 @@
 import test_utils
 
 class UIElements(test_utils.TestCase):
+    fixtures = ['site.json']
 
     def test_we_load_home_template_for_root(self):
         response = self.client.get("/en-US/")
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed('templates/home.html', response)
+        self.assertEquals(4, len(response.context['platforms'].values_list()))
+        self.assertEquals(2, len(response.context['treeinfo'].values_list()))
