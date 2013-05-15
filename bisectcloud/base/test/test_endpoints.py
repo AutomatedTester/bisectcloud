eb453c18655b3581e4209c3fd236327d0ebb2523
AutomatedTester <dburns@mozilla.com>
2013-05-15 13:42:58 +0100

    moving endpoint to a variable for later refactoring

--- bisectcloud/base/test/test_endpoints.py
+++ bisectcloud/base/test/test_endpoints.py
@@ -4,6 +4,7 @@ from bisectcloud.site.models import TaskMaster
 
 class EndPoints(test_utils.TestCase):
     fixtures = ['site.json']
+    add_job = "/en-US/add_job"
 
     def test_we_can_post_payload_to_start_bisect_and_stored_in_database(self):
         data = {
@@ -13,13 +14,13 @@ class EndPoints(test_utils.TestCase):
             "platform": "ubuntu64",
             "tree": "mozilla-central"
         }
-        response = self.client.post("/en-US/add_job", json.dumps(data), content_type='application/json' )
+        response = self.client.post(self.add_job, json.dumps(data), content_type='application/json' )
         self.assertEqual(200, response.status_code)
         self.assertEqual('Task submitted', response.content)
         self.assertEqual(1, len(TaskMaster.objects.filter(bad = '123123123123')))
 
     def test_should_send_an_error_if_a_get_is_done_to_add_job(self):
-        response = self.client.get('/en-US/add_job')
+        response = self.client.get(self.add_job)
         self.assertEqual(200, response.status_code)
         self.assertEqual('GET is not allowed to this endpoint', response.content)
 
@@ -31,7 +32,7 @@ class EndPoints(test_utils.TestCase):
             "platform": "foogbar",
             "tree": "mozilla-central"
         }
-        response = self.client.post('/en-US/add_job', json.dumps(data), content_type='application/json')
+        response = self.client.post(self.add_job, json.dumps(data), content_type='application/json')
         self.assertEqual("Invalid field value in plaform", response.content)
     
     def test_should_receive_error_if_tree_not_in_database(self):
@@ -42,5 +43,5 @@ class EndPoints(test_utils.TestCase):
             "platform": "ubuntu64",
             "tree": "mozilla"
         }
-        response = self.client.post('/en-US/add_job', json.dumps(data), content_type='application/json')
+        response = self.client.post(self.add_job, json.dumps(data), content_type='application/json')
         self.assertEqual("Invalid field value in tree", response.content)
