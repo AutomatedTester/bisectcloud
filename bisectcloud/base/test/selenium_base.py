1644d6cb790b4bf539fbab8968bfd362656ea57e
AutomatedTester <dburns@mozilla.com>
2013-05-26 21:09:05 +0100

    setting up saucelabs

--- bisectcloud/base/test/selenium_base.py
+++ bisectcloud/base/test/selenium_base.py
@@ -1,11 +1,28 @@
+import os
+
 from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+
 
 class BaseTestCase(LiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = WebDriver()
+        try:
+           os.environ['SAUCE_USERNAME']
+           desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
+           desired_capabilities['version'] = os.environ['SAUCE_BROWSER_VERSION']
+           desired_capabilities['platform'] = os.environ['SAUCE_PLATFORM']
+           desired_capabilities['name'] = 'Bisect in the cloud'
+
+           cls.driver = webdriver.Remote(desired_capabilities=desired_capabilities,
+                                          command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % \
+                                          (os.environ['SAUCE_USERNAME'],
+                                          os.environ['SAUCE_ACCESS_KEY'])
+                                         )
+        except Exception as e:
+           print e
+           cls.driver = webdriver.Firefox()
         super(BaseTestCase, cls).setUpClass()
 
     @classmethod
