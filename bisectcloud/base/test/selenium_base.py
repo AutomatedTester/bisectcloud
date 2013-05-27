3e67f1a093df13cafd1e4b1d7dcaf5dcafd57b12
AutomatedTester <dburns@mozilla.com>
2013-05-27 20:21:55 +0100

    updating the travis-ci setup to use sauce from the sauce docs

--- bisectcloud/base/test/selenium_base.py
+++ bisectcloud/base/test/selenium_base.py
@@ -9,17 +9,19 @@ class BaseTestCase(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         try:
-            os.environ['SAUCE_USERNAME']
+            cls.username = os.environ['SAUCE_USERNAME']
+            cls.key = os.environ['SAUCE_ACCESS_KEY']
             desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
             desired_capabilities['version'] = os.environ['SAUCE_BROWSER_VERSION']
             desired_capabilities['platform'] = os.environ['SAUCE_PLATFORM']
             desired_capabilities['name'] = 'Bisect in the cloud'
+            desired_capabilities['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
+            desired_capabilities['build'] = os.environ['TRAVIS_BUILD_NUMBER']
+            desired_capabilities['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
+            hub_url = "%s:%s@localhost:4445" % (cls.username, cls.key)
 
             cls.driver = webdriver.Remote(desired_capabilities=desired_capabilities,
-                                          command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % \
-                                          (os.environ['SAUCE_USERNAME'],
-                                          os.environ['SAUCE_ACCESS_KEY'])
-                                         )
+                                          command_executor="http://%s/wd/hub" % hub_url)
         except Exception as e:
             print ("This following error was encounterd: %s" % e)
             cls.driver = webdriver.Firefox()
